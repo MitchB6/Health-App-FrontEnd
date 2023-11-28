4c71d0851d3d9171ae334fbc1492db0a2ef7b921
Mitchell Butler <mdb484@gmail.com>
2023-11-27 19:05:53 -0500

    log in successful

--- src/pages/login.js
+++ src/pages/login.js
@@ -7,28 +7,50 @@ const Login = ({ onSwitch }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  async function handleLogin(event){
-    event.preventDefault();
 
-    const data = {
-      username: username,
-      password: password
-    };
-    //error handling with try catch
-    try {
-      const response = await api.post('/auth/login', data)
+  // async function handleLogin(event){
+  //   event.preventDefault();
+
+  //   const data = {
+  //     email: email,
+  //     password: password
+  //   };
+  //   //error handling with try catch
+  //   try {
+  //     const response = await api.post('/auth/login', data)
       
-      if (response.ok) {
-        const responseData = await response.json();
-        //handle the response
-        console.log('Login success', responseData)
-      } else  {
-        console.error('Login unsuccessful:', response.status);
+  //     if (response.ok) {
+  //       const responseData = await response.json();
+  //       //handle the response
+  //       console.log('Login success', responseData)
+  //     } else  {
+  //       console.error('Login unsuccessful:', response.status);
+  //     }
+  //   } catch (error){
+  //     console.error('Error during login', error);
+  //   }
+  // }
+  const handleLogin = async () => {
+    setTimeout(async () => {
+      console.log('Login:', email, password);
+      try {
+        const apiUrl = process.env.REACT_APP_API_URL;
+        const response = await axios.post(`${apiUrl}/auth/login`, {
+          email,
+          password
+        });
+        console.log(response);
+        if (response.status === 200) {
+          console.log("Login successful");
+          console.log(response.data);
+        } else {
+          console.log('Login failed');
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (error){
-      console.error('Error during login', error);
-    }
-  }
+    }, 1000);
+  };
 
   return (
     <div className='login-container'>
