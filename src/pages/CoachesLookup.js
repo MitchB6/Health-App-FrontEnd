77cc38cd31447c3e13a89966d0ee03774429821a
Ilona Morhun <ilona@Ilonas-MacBook-Pro.local>
2023-12-02 19:50:30 -0500

    button changes in coach lookup

--- src/pages/CoachesLookup.js
+++ src/pages/CoachesLookup.js
@@ -1,48 +1,52 @@
 import React, { useState } from 'react';
+import mockCoaches from './mockCoachesData'; 
 import './styling/CoachesLookup.css';
-import mockCoaches from './mockCoachesData'; // Import the mock data
 
 const CoachesLookup = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [coaches, setCoaches] = useState(mockCoaches); // Initialize with mock data
+  const [coaches, setCoaches] = useState(mockCoaches); 
 
   const handleSearchChange = (e) => {
-    const query = e.target.value.toLowerCase();
-    setSearchQuery(query);
+    setSearchQuery(e.target.value.toLowerCase());
+  };
 
+  const handleSearchClick = () => {
     const filteredCoaches = mockCoaches.filter(coach =>
-      coach.name.toLowerCase().includes(query) ||
-      coach.location.toLowerCase().includes(query) ||
-      coach.goal.toLowerCase().includes(query) ||
-      coach.cost.toString().toLowerCase().includes(query)
+      coach.name.toLowerCase().includes(searchQuery) ||
+      coach.availability.toLowerCase().includes(searchQuery) ||
+      coach.location.toLowerCase().includes(searchQuery) ||
+      coach.goal.toLowerCase().includes(searchQuery) ||
+      coach.cost.toString().toLowerCase().includes(searchQuery)
     );
 
     setCoaches(filteredCoaches);
   };
 
   return (
-    <div className="container">
-  <div className="search-bar">
-    <input 
-      type="text" 
-      value={searchQuery} 
-      onChange={handleSearchChange} 
-      placeholder="Search by name, availability, location, goal, cost" 
-    />
-    <button className="search-button">Search</button>
-  </div>
-  <div className="coaches-list">
-    {coaches.map((coach) => (
-      <div key={coach.id} className="coach">
-        <h3>{coach.name}</h3>
-        <p>Availability: {coach.availability}</p>
-        <p>Location: {coach.location}</p>
-        <p>Goal: {coach.goal}</p>
-        <p>Cost: ${coach.cost}/hr</p>
+    <div className="coaches-lookup">
+      <div className="search-container"> {/* New wrapper div */}
+        <input 
+          className="search-input"
+          type="text" 
+          value={searchQuery} 
+          onChange={handleSearchChange} 
+          placeholder="Search by name, availability, location, goal, cost" 
+        />
+        <button className="search-button" onClick={handleSearchClick}>Search</button>
       </div>
-    ))}
-  </div>
-</div>
+
+      <div className="coaches-list">
+        {coaches.map((coach) => (
+          <div key={coach.id}>
+            <h3>{coach.name}</h3>
+            <p>Availability: {coach.availability}</p>
+            <p>Location: {coach.location}</p>
+            <p>Goal: {coach.goal}</p>
+            <p>Cost: ${coach.cost}/hr</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
