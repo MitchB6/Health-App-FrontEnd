ed6d2c9b26d600038a03b2e210e833330853abc1
raniya <114628422+RaniyaR@users.noreply.github.com>
2023-12-06 10:25:58 -0500

    added more features

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Home from './pages/home.js';
 import AccountSettings from './pages/AccountSettings.js';
 import CoachesLookup from './pages/CoachesLookup.js'; // Import Coaches Look Up page
 import InitialSurvey from './pages/initialSurvey.js';
-import WeeklyWorkoutPlan from './pages/WeeklyWorkoutPlan.js'
+import WeeklyWorkoutPlan from './pages/WeeklyWorkoutPlan.js';
+import WorkoutNotebook from './pages/WorkoutNotebook.js';
+import PreloadedWorkouts from './pages/PreloadedWorkouts.js';
 
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/weekly-workout-plan" element={<WeeklyWorkoutPlan />} />
+          <Route path="/workout-notebook" element={<WorkoutNotebook />} />
+          <Route path="/preloaded-workouts" element={<PreloadedWorkouts />} />
           <Route path="/signup" element={<Signup onSwitch={handleSwitch} />} />
           <Route path="/login" element={<Login onSwitch={handleSwitch} />} />
           <Route path="/account-settings" element={<AccountSettings />} />
