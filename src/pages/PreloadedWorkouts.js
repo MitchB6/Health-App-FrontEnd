f8b5c2194ac6972e1d1598e5c8482e47b226698c
raniya <114628422+RaniyaR@users.noreply.github.com>
2023-12-20 23:28:16 -0500

    WeeklyWorkoutPlan

--- src/pages/PreloadedWorkouts.js
+++ src/pages/PreloadedWorkouts.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import React from 'react';
 import axios from 'axios';
 import Sidebar from "../components/Sidebar.js";
 import Subcategories from "../components/Subcategories.js";
